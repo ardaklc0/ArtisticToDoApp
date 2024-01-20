90862f6b6c7ccfdd5aa9e9b853bc8c48075b0a30
arda <ardaklc0@gmail.com>
2024-01-20 13:31:49 +0300

    The home page code has been simplified.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,44 +4,40 @@ import 'package:pomodoro2/CreationPage/planner_service.dart';
 import 'package:pomodoro2/GustavKlimtCreation/gustav_klimt.dart';
 import 'package:pomodoro2/OsmanHamdiCreation/osman_hamdi.dart';
 import 'package:pomodoro2/SalvadorDaliCreation/salvador_dali.dart';
-import 'package:pomodoro2/Task/task_service.dart';
 import 'package:pomodoro2/VanGoghCreation/van_gogh.dart';
-import 'package:pomodoro2/VanGoghCreation/van_gogh_variables.dart';
-import 'package:pomodoro2/main.dart';
-
 import 'CreationPage/planner_entity.dart';
 import 'MonetCreation/monet.dart';
 import 'PicassoCreation/picasso.dart';
-
+final Map<String, String> artists =
+{
+  "GustavKlimt":"Gustav Klimt",
+  "OsmanHamdi":"Osman Hamdi Bey",
+  "Monet":"Monet",
+  "Picasso":"Picasso",
+  "SalvadorDali":"Salvador Dali",
+  "VanGogh":"Van Gogh"
+};
+final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+  foregroundColor: Colors.white,
+  backgroundColor: Colors.black,
+);
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
-  late Future<List<Padding>> addCreatedPlanners;
-
+  late Future<List<ElevatedButton>> addCreatedPlanners;
   @override
   void initState(){
     super.initState();
     addCreatedPlanners = fetchPlanners();
   }
-
   @override
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double deviceHeight = MediaQuery.of(context).size.height;
-    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
-      foregroundColor: Colors.white,
-      backgroundColor: Colors.black,
-      padding: EdgeInsets.symmetric(horizontal: deviceWidth * 0.1),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(deviceWidth * 0.05)),
-      ),
-    );
-
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: Center(
@@ -57,75 +53,7 @@ class _HomePageState extends State<HomePage> {
               ),
               SingleChildScrollView(
                 child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: raisedButtonStyle,
-                        onPressed: () {
-                          createPlannerWrtArtist("GustavKlimt");
-                          Navigator.pushNamed(context, '/gustav_klimt');
-                        },
-                        child: const Text('Gustav Klimt'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: raisedButtonStyle,
-                        onPressed: () {
-                          createPlannerWrtArtist("OsmanHamdi");
-                          Navigator.pushNamed(context, '/osman_hamdi');
-                        },
-                        child: const Text('Osman Hamdi Bey'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: raisedButtonStyle,
-                        onPressed: () {
-                          createPlannerWrtArtist("Monet");
-                          Navigator.pushNamed(context, '/monet');
-                        },
-                        child: const Text('Monet'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: raisedButtonStyle,
-                        onPressed: () {
-                          createPlannerWrtArtist("Picasso");
-                          Navigator.pushNamed(context, '/picasso');
-                        },
-                        child: const Text('Picasso'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: raisedButtonStyle,
-                        onPressed: () {
-                          createPlannerWrtArtist("SalvadorDali");
-                          Navigator.pushNamed(context, '/salvador_dali');
-                        },
-                        child: const Text('Salvador Dali'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: raisedButtonStyle,
-                        onPressed: () async {
-                          createPlannerWrtArtist("VanGogh");
-                          Navigator.pushNamed(context, '/van_gogh');
-                        },
-                        child: const Text('Van Gogh'),
-                      ),
-                    ),
-
-                  ],
+                  children: createArtistButton()
                 ),
               ),
               Text(
@@ -139,7 +67,7 @@ class _HomePageState extends State<HomePage> {
                 child: SizedBox(
                   width: deviceWidth * 0.7,
                   height: deviceHeight * 0.3,
-                  child: FutureBuilder<List<Padding>>(
+                  child: FutureBuilder<List<ElevatedButton>>(
                     future: fetchPlanners(),
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
@@ -192,29 +120,43 @@ class _HomePageState extends State<HomePage> {
     );
     await insertPlanner(planner);
   }
-  Future<List<Padding>> fetchPlanners() async {
+  Future<List<ElevatedButton>> fetchPlanners() async {
     var planners = await getPlanners();
-    List<Padding> plannerContainers = [];
-    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
-      foregroundColor: Colors.white,
-      backgroundColor: Colors.black,
-    );
+    List<ElevatedButton> plannerContainers = [];
     for (var element in planners) {
       plannerContainers.add(
-        Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: ElevatedButton(
-            style: raisedButtonStyle,
-            onPressed: () async {
-              goToArtist(element.plannerArtist, element.id!, element.creationDate);
-            },
-            child: Text('${element.id}.) at ${element.creationDate} with ${element.plannerArtist}'),
-          ),
+        ElevatedButton(
+          style: raisedButtonStyle,
+          onPressed: () async {
+            goToArtist(element.plannerArtist, element.id!, element.creationDate);
+          },
+          child: Text('${element.id}.) at ${element.creationDate} with ${element.plannerArtist}'),
         ),
       );
     }
     return plannerContainers;
   }
+  List<ElevatedButton> createArtistButton(){
+    List<ElevatedButton> artistButtons = [];
+    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.black,
+    );
+    artists.forEach((key, value) {
+      artistButtons.add(
+        ElevatedButton(
+          style: raisedButtonStyle,
+          onPressed: () async {
+            await createPlannerWrtArtist(key);
+            setState(() {
+              addCreatedPlanners = fetchPlanners();
+            });
+          },
+          child: Text(value),
+        ),
+      );
+    });
+    return artistButtons;
+  }
 }
-
 
